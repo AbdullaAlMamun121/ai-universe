0fa02755a764d4d0b08acdff4083b0c69dfa2fdc
Abdulla <hmdabdulla121@gmail.com>
2023-03-03 01:18:02 +0600

    set all dynamic data into card

--- js/main.js
+++ js/main.js
@@ -11,24 +11,23 @@ const displayData = (values) => {
     const cardContainer = document.getElementById('card-container');
     values.forEach(value => {
         console.log(value); 
-        const { description, feature, id, image, name, published_in } = value;
+        const { description, features, id, image, name, published_in } = value;
         const card = document.createElement('div');
         card.classList.add('col');
         card.innerHTML = `
                 <div class="card h-100 p-1">
                     <img src="${image ? image :"No image found"}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">Feature</h5>
+                        <p class="card-text">${features}</p>
                     </div>
                     <div class="card-footer">
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="d-flex flex-column">
-                                <h3>chatGpt</h3>
-                                <div class="d-flex g-2">
+                                <h3>${name ? name :"Name not found"}</h3>
+                                <div class="d-flex justify-content-start align-items-center g-2">
                                     <i class="fa-solid fa-calendar-days"></i>
-                                    <p>11/12/2022</p>
+                                    <span class="ms-2 p-0 m-0">${published_in ? published_in :"Date not available"}</span>
                                 </div>
                             </div>
                             <div>
