37a20dea4eb8888d5c13fd1e0b04653f8cd5ab57
Abdulla <hmdabdulla121@gmail.com>
2023-03-03 16:35:05 +0600

    modal desing working and pricing section complete dynamically

--- js/main.js
+++ js/main.js
@@ -1,40 +1,47 @@
 // Fetch all data
+
 const fetchAllData = () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    fetch(url).then(response => response.json()).then(data => displayData(data.data.tools))
+    fetch(url).then(response => response.json()).then(data => {
+        // console.log(data.data.tools);
+        displayData(data.data.tools);
 
+    }
+    )
 }
 
 // Display the fetch all data
 const displayData = (values) => {
-    // console.log(values);
+
     const cardContainer = document.getElementById('card-container');
-     const items = values.slice(0, 12);
-     items.forEach(item => {
-        console.log(item); 
+
+    const items = values.slice(0, 6);
+
+    items.forEach(item => {
+        console.log(item);
+
         const { features, id, image, name, published_in } = item;
         const card = document.createElement('div');
-        
+
         card.classList.add('col');
-        let myFeature = "";
         card.innerHTML = `
                 <div class="card h-100 p-1">
-                    <img src="${image ? image :"No image found"}" class="card-img-top" alt="...">
+                    <img src="${image ? image : "No image found"}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">Features</h5>
                         
-                        <p>${features.map((feature , index) =>
-                            `<br><span>${index+1}.${feature}</span>`
-                        )}</p>
+                        <p>${features ? features.map((feature, index) =>
+            `<br><span>${index + 1}.${feature}</span>`
+        ) : "Feature not available"}</p>
                         
                     </div>
                     <div class="card-footer">
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="d-flex flex-column">
-                                <h3>${name ? name :"Name not found"}</h3>
+                                <h3>${name ? name : "Name not found"}</h3>
                                 <div class="d-flex justify-content-start align-items-center g-2">
                                     <i class="fa-solid fa-calendar-days"></i>
-                                    <span class="ms-2 p-0 m-0">${published_in ? published_in :"Date not available"}</span>
+                                    <span class="ms-2 p-0 m-0">${published_in ? published_in : "Date not available"}</span>
                                 </div>
                             </div>
                             <div>
@@ -47,7 +54,7 @@ const displayData = (values) => {
                 </div>
         `;
         cardContainer.appendChild(card);
-     });
+    });
     // spinner
     toggleSpinner(false);
 }
@@ -57,9 +64,69 @@ const singleDataDetails = (id) => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
     fetch(url).then(response => response.json()).then(data => singleDataDisplay(data.data));
 }
+// modal single data show
 const singleDataDisplay = (values) => {
-    // console.log(values);
+    console.log(values);
+    const modalContainer = document.getElementById('modal-container');
+    const { accuracy, description, features, id, image_link, logo, pricing } = values;
+
+    modalContainer.innerHTML = `
+    <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${accuracy.description}</h5>
+                    <div class="d-flex gap-3 justify-content-center align-items-center ">
+
+                    <div class="bg-white border border-warning rounded p-1 mr-3"><span class="text-warning">${pricing[0].price}<br>${pricing[0].plan}</span>
+                    </div>
+                    <div class="bg-white border border-warning rounded p-1 mr-3"><span class="text-warning">${pricing[1].price}<br>${pricing[1].plan}</span>
+                    </div>
+                    <div class="bg-white border border-warning rounded p-1 mr-3"><span class="text-warning">${pricing[2].price}<br>${pricing[2].plan}</span>
+                    </div>
+                  </div>
+
+                  <div class="d-flex justify-content-center align-items-center gap-3">
+        <div class="p-3">
+            <h3>Feature</h3>
+            <p>Feature list</p>
+            <p>Feature list</p>
+            <p>Feature list</p>
+        </div>
+        <div class="p-3">
+            <h3>Feature</h3>
+            <p>Feature list</p>
+            <p>Feature list</p>
+            <p>Feature list</p>
+        </div>
+    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+
 }
+// sort by date
+// const dateShort = document.getElementById('sort-date');
+// function sortByDate(date) {
+//     // console.log(date);
+//     date.sort((a, b) => new Date(a.date) - new Date(b.date));
+//     return date;
+// }
+// dateShort.addEventListener('click', fetchAllData);
+
+// spinner section
 const toggleSpinner = isLoading => {
     const loadSection = document.getElementById('loader');
     if (isLoading) {
@@ -68,5 +135,8 @@ const toggleSpinner = isLoading => {
         loadSection.classList.add('d-none');
     }
 }
+// see all data section
+
+
 toggleSpinner(true);
 fetchAllData();
